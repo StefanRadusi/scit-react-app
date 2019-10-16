5db4f1bec9f27b8bfd9c74550b8cb135e657c67a
unknown <marta.razvan@gmail.com>
2019-10-16 19:28:12 +0300

    Razvan Marta

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './Header.css';
+import {Menu} from './Menu/Menu';
+import {Profile} from './Profile/Profile';
 
 export function Header() {
   return (
+    
     <div className='header'>
-
-    </div>
+      <Menu/>
+      <Profile/>
+      </div>
   );
 }
 
---
+++ src/components/Header/Menu/Menu.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Menu.css';
+
+export function Menu() {
+    return (
+        <div className='menu-container'>
+            <div className='menu-button' id='menu-button'>
+                <img src='https://www.stickpng.com/assets/images/588a6507d06f6719692a2d15.png'></img>
+            </div>
+        </div>
+  
+    );
+  }
+  
+  
